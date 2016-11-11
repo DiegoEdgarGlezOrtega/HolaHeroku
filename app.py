b64fabb13640bedbf8bc7486f94fa2d2125ce4c2
DiegoEdgarGlezOrtega <1714110120@utec-tgo.edu.mx>
2016-11-11 14:20:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,15 @@
 import web
 
-render = web.template.render('/')
+render = web.template.render('')
 urls = (
-    '/home','index'
+    '/','index'
     #,'/variables(.*)','variables',
     #'/parametro(.*)','parametro',
 )
 class index:
     def GET (self):
         return render.index()
- '''
-class variables:
-    def GET(self,nombre):
-        nombre = 'Dieguin'
-        return render.variables(nombre)
-
-class parametro:
-    def GET(self,nombre):
-        i = web.input(nombre=None)
-
-        return render.parametro(i.nombre) 
- '''
+ 
 if __name__ == '__main__':
     app = web.application(urls,globals())
     web.config.debug = True
